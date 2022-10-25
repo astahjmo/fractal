8c038fdca663bb88433da01aa29938f49d155bd6
johmatos <contato.johnatan06@gmail.com>
2022-10-25 20:05:47 +0200

    Fix norminette errors

--- src/parse.c
+++ src/parse.c
@@ -6,16 +6,16 @@
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 22:25:48 by johmatos          #+#    #+#             */
-/*   Updated: 2022/10/25 19:23:42 by johmatos         ###   ########.fr       */
+/*   Updated: 2022/10/25 20:05:25 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fractal.h"
 #include <stdio.h>
 
-static int	  check_parms(char *re, char*im)
+static int	check_parms(char *re, char*im)
 {
-	int	  len;
+	int	len;
 
 	len = ft_strlen(re);
 	while (len != 0)
@@ -42,7 +42,7 @@ int	parser(char *str, char *re, char *im)
 		&& re == NULL && im == NULL)
 		program_init(MANDELBROT, 0, 0);
 	else if (ft_strncmp("julia", str, ft_strlen("julia")) == 0
-		&& re != NULL && im != NULL && check_parms(re, im) ==  0)
+		&& re != NULL && im != NULL && check_parms(re, im) == 0)
 		program_init(JULIA, ft_atof(re), ft_atof(im));
 	else
 	{
